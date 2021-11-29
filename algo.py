fe5f8c52c928c05ac518805db2e4e8e5c4bb3434
Tejas Mandre <tejasmandre0306@gmail.com>
2021-11-29 11:39:07 +0530

    initial commit

---
+++ algo.py
@@ -0,0 +1,22 @@
+class CaesarAPI:
+
+    def encrypty(self, plaintext, key):
+        pass
+
+    def encrypty(self, ciphertext, key):
+        pass
+
+    def cryptanalyse(self, ciphertext):
+        pass
+
+
+class ExtendedExperimentalAPI:
+
+    pass
+
+
+class RandomPasswordGeneratorAPI:
+
+    def generate(self, length):
+        pass
+
